9fb99c16a30bee46160752f4694b7df81ffd3fb1
Nicolas Kobal <nicolas.hacala@gmail.com>
2019-04-03 10:32:13 +0100

    Updates

---
+++ precache-manifest.4b9dd44deebd79a0566a10e25b90f6ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "912903dc63fd2aa66894",
+    "url": "/contact_manager-REACT/static/js/runtime~main.8e3f2b3f.js"
+  },
+  {
+    "revision": "88bc35103520d0f5942d",
+    "url": "/contact_manager-REACT/static/js/main.0c53d551.chunk.js"
+  },
+  {
+    "revision": "7426e762b1eedfc562e2",
+    "url": "/contact_manager-REACT/static/js/2.f1b0d87e.chunk.js"
+  },
+  {
+    "revision": "88bc35103520d0f5942d",
+    "url": "/contact_manager-REACT/static/css/main.e53aff8f.chunk.css"
+  },
+  {
+    "revision": "7426e762b1eedfc562e2",
+    "url": "/contact_manager-REACT/static/css/2.d3d5260d.chunk.css"
+  },
+  {
+    "revision": "b7d1019325fe5428f2fa7c60e8d279be",
+    "url": "/contact_manager-REACT/index.html"
+  }
+];
